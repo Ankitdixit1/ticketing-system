32dbe58153b723e4203aec44e0c8d31b6626e91e
Ankitdixit1 <ankitdixit29269@gmail.com>
2026-08-30 18:02:56 +0530

    adding more

--- src/app.js
+++ src/app.js
@@ -9,5 +9,12 @@ app.get("/api/health", (req, res) => {
         serverice: "ticketing service",
     });
 });
+
+// error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(500).json({ error: "Something went wrong!" });
+});
+
 // exports the app to be used in server.js
 module.exports = app;
--- src/server.js
+++ src/server.js
@@ -1,13 +1,6 @@
 const app = require("./app");
 const PORT = 4000;
 
-
 app.listen(PORT, () => {
-    console.log(`server is running on port ${PORT}`);
-});
-
-// error handling middleware
-app.use((err, req, res, next) => {
-    console.log(err);
-    res.status(500).json({ error: "Something went wrong!" });
+    console.log(`Server is running on port ${PORT}`);
 });
